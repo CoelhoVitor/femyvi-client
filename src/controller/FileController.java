88c5651bafb7ec122fb32fb2712a28cc09f8d293
CoelhoVitor <vitorvcs@hotmail.com>
2019-11-16 21:18:26 -0300

    fix filename name from Controller

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -36,11 +36,11 @@ public class FileController {
         // create file message
         FileMessage fm = new FileMessage();
 
-        String fileName = file.getName();
+        String filename = file.getName();
         Path path = file.toPath();
 
-        fm.setFilename(fileName);
-        fm.setFileType(FileUtils.getExtension(fileName));
+        fm.setFilename(filename);
+        fm.setFileType(FileUtils.getExtension(filename));
         fm.setOriginPath(file.getAbsolutePath());
         fm.setCreatedDate(FileUtils.getCreationDate(path));
         fm.setContent(FileUtils.getContent(path));
